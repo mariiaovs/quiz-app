2cd83dde7adc5d6d254e2828a0a5f355be8e1eeb
Mariia Posselt <maruw@ya.ru>
2023-12-12 10:45:06 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const questions = [
+  {
+    id: 1,
+    question: "Welche Teesorte stammt ursprünglich aus China?",
+    answer: "Grüner Tee",
+    tags: ["grüntee", "china"],
+  },
+
+  {
+    id: 2,
+    question: "Welches ist das meistgetrunkene Getränk der Welt nach Wasser?",
+    answer: "Schwarzer Tee",
+    tags: ["schwarztee", "wasser"],
+  },
+
+  {
+    id: 3,
+    question:
+      "Welche Region ist bekannt für die Produktion von Darjeeling-Tee?",
+    answer: "Indien",
+    tags: ["indien"],
+  },
+
+  {
+    id: 4,
+    question:
+      "Was verleiht Schwarzem Tee seine dunkle Farbe und kräftigen Geschmack?",
+    answer: "Fermentation",
+    tags: ["indien"],
+  },
+];
