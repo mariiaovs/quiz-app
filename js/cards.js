4c25f749c3ee00380a592497cd905f9a1bc89d36
Mariia Posselt <maruw@ya.ru>
2024-01-15 11:30:07 +0100

    change array with questions

--- js/cards.js
+++ js/cards.js
@@ -9,29 +9,36 @@ const questions = [
       "D: Kamillentee",
     ],
     rightAnswer: 0,
-    tags: ["tee", "china"],
+    tags: ["teesorten", "china"],
+    isBookmarked: false,
   },
 
   {
     id: 2,
     question: "Welches ist das meistgetrunkene Getränk der Welt nach Wasser?",
-    answer: "Schwarzer Tee",
-    tags: ["schwarztee", "wasser"],
+    answers: ["Kaffee", "Orangensaft", "Schwarzer Tee", "Cola"],
+    rightAnswer: 2,
+    tags: ["teesorten", "wasser"],
+    isBookmarked: false,
   },
 
   {
     id: 3,
     question:
       "Welche Region ist bekannt für die Produktion von Darjeeling-Tee?",
-    answer: "Indien",
-    tags: ["indien"],
+    answers: ["China", "Indien", "Japan", "Sri-Lanka"],
+    tags: ["region", "teesorten"],
+    rightAnswer: 1,
+    isBookmarked: false,
   },
 
   {
     id: 4,
     question:
       "Was verleiht Schwarzem Tee seine dunkle Farbe und kräftigen Geschmack?",
-    answer: "Fermentation",
-    tags: ["schwarztee"],
+    answers: ["Fermentation", "Sonnenlicht", "Kräuterzusätze", "Zitronensaft"],
+    rightAnswer: 0,
+    tags: ["schwarztee", "teesorten"],
+    isBookmarked: false,
   },
 ];
