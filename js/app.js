42930b14348d5fc62b65f1a39d2223059d5b6b81
Mariia Posselt <maruw@ya.ru>
2024-02-24 11:54:33 +0100

    several tags in input, createCards function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-import { cards } from "../js/cards.js";
+import { cards } from "../js/state.js";
+import createCard from "./createCard.js";
 
 let currentTheme = localStorage.getItem("theme") || "light";
 
@@ -7,77 +8,5 @@ if (currentTheme === "dark") document.body.setAttribute("data-theme", "dark");
 const main = document.querySelector('[data-js="cards"]');
 
 cards.forEach((card) => {
-  const cardElement = document.createElement("main");
-  cardElement.classList.add("card");
-
-  const cardTitleElement = document.createElement("h2");
-  cardTitleElement.classList.add("card_title");
-  cardTitleElement.textContent = `Question ${card.id}`;
-
-  const bookmarkButton = document.createElement("button");
-  bookmarkButton.className = "button bookmark";
-  bookmarkButton.setAttribute("aria-label", "bookmark");
-  bookmarkButton.innerHTML = `<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  width="2rem"
-  height="2rem"
->
-  <polygon
-    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-  />
-</svg>`;
-
-  bookmarkButton.addEventListener("click", () => {
-    card.isBookmarked = !card.isBookmarked;
-    bookmarkButton.classList.toggle("clicked");
-  });
-
-  const cardQuestion = document.createElement("p");
-  cardQuestion.classList.add("question");
-  cardQuestion.textContent = card.question;
-
-  const showAnswerButton = document.createElement("button");
-  showAnswerButton.className = "card_button button";
-  showAnswerButton.textContent = "Show answer";
-
-  const cardAnswer = document.createElement("p");
-  cardAnswer.classList.add("answer");
-  cardAnswer.setAttribute("hidden", "");
-  cardAnswer.textContent = card.answers[card.rightAnswer];
-
-  showAnswerButton.addEventListener("click", () => {
-    if (showAnswerButton.textContent.includes("Hide")) {
-      showAnswerButton.textContent = "Show answer";
-      cardAnswer.setAttribute("hidden", "");
-    } else {
-      showAnswerButton.textContent = "Hide answer";
-      cardAnswer.removeAttribute("hidden");
-    }
-  });
-
-  const cardTagsList = document.createElement("ul");
-  cardTagsList.classList.add("card_tags");
-  card.tags.forEach((tag) => {
-    const cardTag = document.createElement("li");
-    cardTag.setAttribute("aria-label", `${tag}-hashtag`);
-    cardTag.textContent = `#${tag}`;
-    cardTagsList.append(cardTag);
-  });
-
-  cardElement.append(
-    cardTitleElement,
-    bookmarkButton,
-    cardQuestion,
-    showAnswerButton,
-    cardAnswer,
-    cardTagsList
-  );
-  main.append(cardElement);
+  main.append(createCard(card));
 });
-
-const showAnswerButton = document.querySelector('[data-js="show-answer"]');
-const answerText = document.querySelector('[data-js="answer-text"]');
-console.log(answerText);
-
-let answerIsShown = false;
--- js/form.js
+++ js/form.js
@@ -1,51 +1,9 @@
+import createCard from "./createCard.js";
+import { cards } from "./state.js";
+
 let currentTheme = localStorage.getItem("theme") || "light";
 
 if (currentTheme === "dark") document.body.setAttribute("data-theme", "dark");
-
-let questions = [
-  {
-    id: 1,
-    question: "Welche Teesorte stammt ursprünglich aus China?",
-    answers: [
-      "A: Grüner Tee",
-      "B: Schwarzer Tee",
-      "C: Rooibos-Tee",
-      "D: Kamillentee",
-    ],
-    rightAnswer: 0,
-    tags: ["teesorten", "china"],
-    isBookmarked: false,
-  },
-
-  {
-    id: 2,
-    question: "Welches ist das meistgetrunkene Getränk der Welt nach Wasser?",
-    answers: ["Kaffee", "Orangensaft", "Schwarzer Tee", "Cola"],
-    rightAnswer: 2,
-    tags: ["teesorten", "wasser"],
-    isBookmarked: false,
-  },
-
-  {
-    id: 3,
-    question:
-      "Welche Region ist bekannt für die Produktion von Darjeeling-Tee?",
-    answers: ["China", "Indien", "Japan", "Sri-Lanka"],
-    tags: ["region", "teesorten"],
-    rightAnswer: 1,
-    isBookmarked: false,
-  },
-
-  {
-    id: 4,
-    question:
-      "Was verleiht Schwarzem Tee seine dunkle Farbe und kräftigen Geschmack?",
-    answers: ["Fermentation", "Sonnenlicht", "Kräuterzusätze", "Zitronensaft"],
-    rightAnswer: 0,
-    tags: ["schwarztee", "teesorten"],
-    isBookmarked: false,
-  },
-];
 
 const form = document.querySelector('[data-js="form"]');
 const main = document.querySelector('[data-js="main"]');
@@ -71,86 +29,21 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const data = Object.fromEntries(new FormData(event.target));
 
-  let newQuestion = {
-    id: questions.length + 1,
+  let tags = data.tags.split(",");
+  tags.map((tag) => tag.trim());
+
+  let newCard = {
+    id: cards.length + 1,
     question: data.question,
     answers: [data.answer, "Orangensaft", "Schwarzer Tee", "Cola"], // mix answers?
     rightAnswer: 0,
-    tags: [data.tag],
+    tags,
     isBookmarked: false,
   };
 
-  questions.push(newQuestion);
+  cards.push(newCard);
 
-  const newCard = document.createElement("section");
-  newCard.classList.add("card");
-
-  const newCardTitle = document.createElement("h2");
-  newCardTitle.classList.add("card_title");
-  newCardTitle.textContent = `Question ${newQuestion.id}`;
-
-  const newCardBookmark = document.createElement("button");
-  newCardBookmark.classList.add("button", "bookmark");
-  newCardBookmark.setAttribute("aria-label", "bookmark");
-  newCardBookmark.innerHTML = `<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  width="2rem"
-  height="2rem"
->
-  <polygon
-    points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-  />
-</svg>`;
-
-  newCardBookmark.addEventListener("click", () => {
-    newCardBookmark.classList.toggle("clicked");
-  });
-
-  const newCardQuestion = document.createElement("p");
-  newCardQuestion.classList.add("question");
-  newCardQuestion.textContent = data.question;
-
-  const newShowAnswerButton = document.createElement("button");
-  newShowAnswerButton.classList.add("card_button", "button");
-  newShowAnswerButton.textContent = "Show answer";
-
-  const newCardAnswer = document.createElement("p");
-  newCardAnswer.classList.add("answer");
-  newCardAnswer.setAttribute("hidden", "");
-  newCardAnswer.textContent = data.answer;
-
-  const newCardTags = document.createElement("ul");
-  newCardTags.classList.add("card_tags");
-
-  const newCardTag = document.createElement("li");
-  newCardTag.setAttribute("aria-label", "tee-hashtag");
-  newCardTag.textContent = `#${data.tag}`;
-
-  let answerIsShown = false;
-
-  newShowAnswerButton.addEventListener("click", () => {
-    if (answerIsShown) {
-      newShowAnswerButton.textContent = "Show answer";
-      newCardAnswer.setAttribute("hidden", "");
-    } else {
-      newShowAnswerButton.textContent = "Hide answer";
-      newCardAnswer.removeAttribute("hidden");
-    }
-    answerIsShown = !answerIsShown;
-  });
-
-  newCardTags.append(newCardTag);
-  newCard.append(
-    newCardTitle,
-    newCardBookmark,
-    newCardQuestion,
-    newShowAnswerButton,
-    newCardAnswer,
-    newCardTags
-  );
-
-  main.append(newCard);
+  main.append(createCard(newCard));
   event.target.reset();
   event.target.elements.question.focus();
 });
